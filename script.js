99b54b6e363958dff178af6719833371d10bcd70
offxsaint <211593962+offxsaint@users.noreply.github.com>
2026-04-14 20:13:14 +0200

    Function to clear expression. Buttons to add expression visualy.

--- script.js
+++ script.js
@@ -1,29 +1,65 @@
+let currentexpression = "";
+
 function Operator() {
-    const add =  (a, b) => {
-        return a + b;
-    }
-    const subtract = (a, b) => {
-        return a - b;
-    }
+    const add =  (a, b) => a + b;
+    const subtract = (a, b) => a - b;
     const divide = (a, b) => {
         if(b == 0) {
-            throw Error;
+            throw new Error("Cannot not dividde by zero!");
         }
         return a / b;
     }
-    const multiply = (a, b) => {
-        return a * b;
-    }
-    return {
+
+    const multiply = (a, b) => a * b;
+
+    const operators = {
         "+": add,
         "-": subtract,
         "/": divide,
         "*": multiply
     };
 
-    function calculate(expression) {
-        const [a, op, b] = expression.split(" ")
-        return this[this.getOperatorName(op)](parseFloat(a), parseFloat(b));
+    return { 
+        calculate(expression) {
+        const [a, op, b] = expression;
+        return operators[op](parseFloat(a), parseFloat(b));
+        }
     };
-
 }
+
+const calculator = Operator();
+
+function addtoexpression(value) {
+    if(value === "x") value = "*";
+    if(value === "&divide;") value = "/";
+
+    currentexpression += value;
+    document.querySelector(".expression").innerHTML = currentexpression || "Click buttons to build Expression!";
+    document.querySelector(".result").innerHTML = "";
+}
+
+function clearexpression() {
+    currentexpression = "";
+    document.querySelector(".expression").innerHTML = "Click buttons to build Expression";
+    document.querySelector(".result").innerHTML = "";
+}
+
+function calculateexpression() {
+    if(!currentexpression) {
+        document.querySelector(".result").innerHTML = "Enter an Expression!";
+        return;
+    }
+
+    try {
+        const result = calculator.calculate(currentexpression);
+        document.querySelector(".result").innerHTML = `${currentexpression} = ${result}`;
+    }
+    catch (error) {
+        document.querySelector(".result").innerHTML = `Error`
+    };
+    
+}
+
+
+
+
